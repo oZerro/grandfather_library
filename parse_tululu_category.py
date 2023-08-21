65ca598754d10824ec4b1940a204cfe4610b7fd4
oZerro <leha.normatov@mail.ru>
2023-08-21 20:34:15 +0300

    download books from all pages

---
+++ parse_tululu_category.py
@@ -0,0 +1,51 @@
+import requests
+import json
+from requests.exceptions import HTTPError, ConnectionError
+from bs4 import BeautifulSoup
+from save_book import check_for_redirect, download_image, download_txt, parse_book_page
+from urllib.parse import urljoin
+
+
+
+if __name__ == "__main__":
+    main_url = "https://tululu.org/"
+    download_url = "https://tululu.org/txt.php"
+    page_num = 4
+    count = 1
+    for page in range(1, page_num + 1):
+        fantasy_block_url = f"https://tululu.org/l55/{page}/"
+        response = requests.get(fantasy_block_url)
+        response.raise_for_status()
+        check_for_redirect(response)
+
+        soup = BeautifulSoup(response.text, "lxml")
+        book_cards = soup.find_all("table", class_="d_book")
+        for book_url in book_cards:
+            book_id = book_url.find("a")["href"][2:-1]
+            book_url = urljoin(main_url, book_url.find("a")["href"])
+            params = {
+                "id": book_id
+            }
+            try:
+                response = requests.get(book_url)
+                response.raise_for_status()
+                check_for_redirect(response)
+
+                reference_book = parse_book_page(response.text, book_url)
+                download_txt(download_url, params, f'{book_id}.{reference_book["book_name"]}')
+                download_image(reference_book['full_img_url'], reference_book['short_img_url'])
+                
+                with open("capitals.json", "a", encoding="utf-8") as file:
+                    json.dump(reference_book, file, ensure_ascii=False, indent=2)
+                    file.write("\n\n")
+            except HTTPError as ex:
+                print("На данной странице нет книги.", end="\n\n")
+            except ConnectionError as ex:
+                print(ex)
+
+            print(count)
+            count += 1
+
+
+    
+
